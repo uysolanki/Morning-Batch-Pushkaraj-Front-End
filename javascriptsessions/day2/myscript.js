e9b83a95ec862ed57cac396d94e63e84f06428e5
Umar Solanki <uysolanki@gmail.com>
2024-11-06 09:58:58 +0530

    rps 1

---
+++ javascriptsessions/day2/myscript.js
@@ -0,0 +1,9 @@
+function displayInstitueName()
+{
+    alert('Welcome to ExclerR Edtech Pvt ltd, Pune India');
+}
+
+function displayCourseName()
+{
+    alert('Welcome to FullStack Development Java+SQL+React');
+}
