3500582370690da558c4b3ac6c05a59420395657
Umar Solanki <uysolanki@gmail.com>
2024-11-26 11:17:29 +0530

    day 5

--- reactsessions/project3/src/components/shared/NavList.jsx
+++ reactsessions/project3/src/components/shared/NavList.jsx
@@ -1,11 +1,18 @@
 import React from "react";
-import './NavList.css'
+import "./NavList.css";
 const NavList = () => {
+  const navListItems = ["TextReader", "Calculator", "App3", "App4", "App5"];
+
   return (
     <ul>
-      <li>TextReader</li>
-      <li>Calculator</li>
-      <li>App3</li>
+      {
+      navListItems.map(
+        (navListItem, index) => 
+        {
+        return <li key={index}>{navListItem}</li>;
+        }
+      )
+      }
     </ul>
   );
 };
